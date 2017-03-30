3f3beefa2e9ca27dc634b9381acc1e194a735fd7
lizhiming <458882786@qq.com>
2017-03-30 21:09:23 +0800

    页面顶部基本完成

--- src/js/config.js
+++ src/js/config.js
@@ -5,28 +5,25 @@ requirejs.config({
 	// 设置别名
 	// 格式：别名:真实路径
 	paths:{
-		'jquery':'../lib/jquery-3.1.1'
+		'jquery':'../assets/jquery-3.1.1'
 	},
 
 	shim:{
-		'ajax':{
-			exports:'ajax'
-		},
-		'jQuery.prototype.gdszoom':{
-			deps: ["jquery"],
-			exports:'jQuery.prototype.gdszoom'
-		}
+		// 'ajax':{
+		// 	exports:'ajax'
+		// },
+		// 'jQuery.prototype.gdszoom':{
+		// 	deps: ["jquery"],
+		// 	exports:'jQuery.prototype.gdszoom'
+		// }
 	}
 });
 
 // 引入其他模块
 // 在requireJS中，一个模块就是一个js文件
 // 引入模块的路径：
-requirejs(['jquery','common','home','ajax','jquery.gdszoom'],function($,common,home,myajax,gdszoom){
-	home();
-	common.animate();
-	console.log($);
-
+requirejs(['jquery','home'],function($,home){
+    home.getdata();
 	//$('#box');
 
 	//console.log(gdszoom)
